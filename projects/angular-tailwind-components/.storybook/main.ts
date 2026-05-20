7fa3e2a9e81eb6b9d42ec521a1123ec1dcf7dad5
Giuseppe Morale <morale-giuseppe@live.it>
2026-05-20 23:36:01 +0200

    Release 1.6.3

--- projects/angular-tailwind-components/.storybook/main.ts
+++ projects/angular-tailwind-components/.storybook/main.ts
@@ -4,14 +4,30 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const publicDir = join(__dirname, '../../../public');
+const isStackblitz = process.env['STACKBLITZ'] === 'true';
 
 const config: StorybookConfig = {
-  stories: ['../../../storybook/**/*.mdx', '../../../storybook/**/*.stories.@(ts|tsx)'],
+  stories: isStackblitz
+    ? ['../../../storybook/**/*.stories.ts']
+    : ['../../../storybook/**/*.mdx', '../../../storybook/**/*.stories.@(ts|tsx)'],
   staticDirs: [{ from: publicDir, to: '/' }],
-  addons: ['@storybook/addon-docs', '@storybook/addon-a11y'],
+  addons: isStackblitz
+    ? ['@storybook/addon-docs']
+    : ['@storybook/addon-docs', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/angular',
     options: {}
+  },
+  webpackFinal: async (config) => {
+    if (!isStackblitz) {
+      return config;
+    }
+
+    config.plugins = (config.plugins ?? []).filter(
+      (plugin) => plugin?.constructor?.name !== 'ForkTsCheckerWebpackPlugin'
+    );
+
+    return config;
   }
 };
 
--- projects/angular-tailwind-components/README.md
+++ projects/angular-tailwind-components/README.md
@@ -277,7 +277,7 @@ ng build angular-tailwind-components
 # Start Storybook (locale)
 npm run storybook
 
-# Storybook per StackBlitz (CI, host 0.0.0.0, porta 6006)
+# StackBlitz: build statico Storybook + serve (primo avvio ~1–2 min)
 npm run stackblitz
 ```
 

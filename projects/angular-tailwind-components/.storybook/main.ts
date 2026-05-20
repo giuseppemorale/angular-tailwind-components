06683fa8921d60208d6dd6e59c7396012bf040fe
Giuseppe Morale <morale-giuseppe@live.it>
2026-05-21 00:00:11 +0200

    Revert "Release 1.6.3"
    This reverts commit 7fa3e2a9e81eb6b9d42ec521a1123ec1dcf7dad5.

--- projects/angular-tailwind-components/.storybook/main.ts
+++ projects/angular-tailwind-components/.storybook/main.ts
@@ -4,30 +4,14 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const publicDir = join(__dirname, '../../../public');
-const isStackblitz = process.env['STACKBLITZ'] === 'true';
 
 const config: StorybookConfig = {
-  stories: isStackblitz
-    ? ['../../../storybook/**/*.stories.ts']
-    : ['../../../storybook/**/*.mdx', '../../../storybook/**/*.stories.@(ts|tsx)'],
+  stories: ['../../../storybook/**/*.mdx', '../../../storybook/**/*.stories.@(ts|tsx)'],
   staticDirs: [{ from: publicDir, to: '/' }],
-  addons: isStackblitz
-    ? ['@storybook/addon-docs']
-    : ['@storybook/addon-docs', '@storybook/addon-a11y'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/angular',
     options: {}
-  },
-  webpackFinal: async (config) => {
-    if (!isStackblitz) {
-      return config;
-    }
-
-    config.plugins = (config.plugins ?? []).filter(
-      (plugin) => plugin?.constructor?.name !== 'ForkTsCheckerWebpackPlugin'
-    );
-
-    return config;
   }
 };
 
